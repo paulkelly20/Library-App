4857ec66ea8340c560798e8c039456410840cc3b
Paul Kelly <paul.kelly20@gmail.com>
2018-05-23 15:14:08 +0100

    added controllers to library file

--- src/main/java/controllers/LibraryController.java
+++ src/main/java/controllers/LibraryController.java
@@ -1,11 +1,16 @@
 package controllers;
 
+import models.Book;
+
 import static spark.SparkBase.staticFileLocation;
 
 public class LibraryController {
 
 public static void main(String[] args){
     staticFileLocation("/public");
+
+    BorrowerController borrowerController = new BorrowerController();
+    BookController bookController = new BookController();
 
 }
 }
