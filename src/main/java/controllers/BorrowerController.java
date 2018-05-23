4023206537207434370c3da89ccdcc7afb990dd8
Paul Kelly <paul.kelly20@gmail.com>
2018-05-23 15:06:37 +0100

    added show method

--- src/main/java/controllers/BorrowerController.java
+++ src/main/java/controllers/BorrowerController.java
@@ -2,6 +2,8 @@ package controllers;
 
 import db.DBHelper;
 import models.Book;
+import models.Borrower;
+import models.Library;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 
@@ -13,14 +15,21 @@ import static spark.SparkBase.staticFileLocation;
 
 public class BorrowerController {
 
-    public static void main(String[] args) {
-        staticFileLocation("/public");
+    public BorrowerController() {
+        setUpEndPoints();
+    }
+    public void setUpEndPoints()
+    {
+
+        LibraryController libraryController = new LibraryController();
+
         VelocityTemplateEngine velocityTemplateEngine = new VelocityTemplateEngine();
 
-        get("/books", (req, res) -> {
-            List<Book> books = DBHelper.getAll(Book.class);
+        get("/borrowers", (req, res) -> {
+            List<Borrower> borrowers = DBHelper.getAll(Borrower.class);
             HashMap<String, Object> model = new HashMap<>();
-            model.put("result", );
+            model.put("borrowers", borrowers);
+            model.put("template", "templates/borrowers/index.vtl");
             return new ModelAndView(model, "layout.vtl");
         }, velocityTemplateEngine);
     }
