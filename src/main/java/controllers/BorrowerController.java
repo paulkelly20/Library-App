660fb83ef08c56bc8b0d517a046d6e5c8be4fde6
Paul Kelly <paul.kelly20@gmail.com>
2018-05-23 14:40:28 +0100

    Added layout

--- src/main/java/controllers/BorrowerController.java
+++ src/main/java/controllers/BorrowerController.java
@@ -1,4 +1,27 @@
 package controllers;
 
+import db.DBHelper;
+import models.Book;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+
+import java.util.HashMap;
+import java.util.List;
+
+import static spark.Spark.get;
+import static spark.SparkBase.staticFileLocation;
+
 public class BorrowerController {
+
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+        VelocityTemplateEngine velocityTemplateEngine = new VelocityTemplateEngine();
+
+        get("/books", (req, res) -> {
+            List<Book> books = DBHelper.getAll(Book.class);
+            HashMap<String, Object> model = new HashMap<>();
+            model.put("result", );
+            return new ModelAndView(model, "layout.vtl");
+        }, velocityTemplateEngine);
+    }
 }
